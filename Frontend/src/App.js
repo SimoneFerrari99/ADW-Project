3fa362999b6ca909d60ba04aedef791ff5fc1922
Simone Ferrari <simone99ferrari99@gmail.com>
2022-06-23 19:50:32 +0200

    Started playing with frontend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 import Button from "@mui/material/Button";
+import MenuAppBar from "./components/layout/MenuAppBar";
+import Container from "./components/layout/AppContainer";
 
-function App() {
-	return <Button variant="contained">Hello World</Button>;
+import { React, Fragment } from "react";
+
+export default function App() {
+	return (
+		<Fragment>
+			<MenuAppBar></MenuAppBar>
+			<Container maxWidth="xl">
+				<Button variant="contained">Hello World</Button>
+			</Container>
+		</Fragment>
+	);
 }
-
-export default App;
