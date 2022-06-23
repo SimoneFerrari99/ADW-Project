0910406dc03fbcdaa4075182c8f5bb5da1e7c6b3
Simone Ferrari <simone99ferrari99@gmail.com>
2022-06-23 13:08:33 +0200

    Backend Refactoring

--- Backend/src/main/java/com/adwProject/Backend/resolver/CustomerResolver.java
+++ Backend/src/main/java/com/adwProject/Backend/resolver/CustomerResolver.java
@@ -3,7 +3,6 @@ package com.adwProject.Backend.resolver;
 import com.adwProject.Backend.domanin.Agent;
 import com.adwProject.Backend.domanin.Customer;
 import com.adwProject.Backend.domanin.Order;
-import com.adwProject.Backend.entity.CustomerDB;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -12,12 +11,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class CustomerResolver implements GraphQLQueryResolver {
-<<<<<<< Updated upstream
-
-    public Customer customerById(UUID id) {
-=======
     public Customer customerById(long id) {
->>>>>>> Stashed changes
         log.info("Retrieving customer id: {}", id);
 
         return Customer.builder()
