0179328fbc3e5282f4ea1f9f8d88163c09e419a4
SimoneFerrari99 <simone99ferrari99@gmail.com>
2022-06-25 16:46:08 +0200

    Create Datatable

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,11 @@
 import MenuAppBar from "./components/layout/MenuAppBar";
 import { Container } from "@mui/material";
 import { CssBaseline } from "@mui/material";
+import Box from "@mui/material/Box";
 
 import { React, Fragment } from "react";
 import AmountRow from "./components/layout/AmountRow";
+import DataTable from "./components/layout/Table";
 
 export default function App() {
 	return (
@@ -12,8 +14,13 @@ export default function App() {
 			<MenuAppBar />
 			<main>
 				<div>
-					<Container maxWidth="xl" sx={{ mt: { xs: 2, sm: 3 } }}>
-						<AmountRow />
+					<Container maxWidth="xl">
+						<Box sx={{ mt: { xs: 2, sm: 3 } }}>
+							<AmountRow />
+						</Box>
+						<Box sx={{ mt: { xs: 2, sm: 3 } }}>
+							<DataTable />
+						</Box>
 					</Container>
 				</div>
 			</main>
