5f8db158b1ee582d5a13de18850586fe0d7953ed
tulio98 <casarotti30@gmail.com>
2022-06-26 15:24:19 +0200

    Added filter for CORS

--- Backend/src/main/java/com/adwProject/Backend/BackendApplication.java
+++ Backend/src/main/java/com/adwProject/Backend/BackendApplication.java
@@ -1,13 +1,12 @@
 package com.adwProject.Backend;
 
-import com.adwProject.Backend.entity.Customer;
-import com.adwProject.Backend.repository.CustomerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
