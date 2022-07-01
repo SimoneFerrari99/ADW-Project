08d787222170854ba98ee3a58ed32711b2e106ec
Simone Ferrari <simone99ferrari99@gmail.com>
2022-07-01 19:24:00 +0200

    little data changes

--- Backend/src/main/resources/dataDBUsers.sql
+++ Backend/src/main/resources/dataDBUsers.sql
@@ -13,7 +13,7 @@ INSERT INTO USERS (CODE, TYPOLOGY,ACTIVE,EMAIL,PW)
 VALUES
 ('C00001', 'C', 'true', 'cliente@gmail.com', 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86'),
 ('A001', 'A', 'true', 'agente@gmail.com', 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86'),
-('D001', 'D', 'true', 'dirigente@gmail.com', 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86');
+('A002', 'D', 'true', 'dirigente@gmail.com', 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86');
 
 SELECT *
 FROM users;
