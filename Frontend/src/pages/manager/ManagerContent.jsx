26280d1f3155b2e4825c25941103fd47ac3a61b3
SimoneFerrari99 <simone99ferrari99@gmail.com>
2022-07-03 15:01:01 +0200

    Prepared Manager Content

--- Frontend/src/pages/manager/ManagerContent.jsx
+++ Frontend/src/pages/manager/ManagerContent.jsx
@@ -1,13 +1,13 @@
 import { Container, Box } from "@mui/material";
 
-import AgentOrdersTable from "../agent/components/AgentOrdersTable";
+import ManagerOrdersTable from "../manager/components/ManagerOrdersTable";
 
 export default function ManagerContent() {
 	return (
 		<main>
 			<Container maxWidth="xl">
 				<Box sx={{ mt: { xs: 2, sm: 3 } }}>
-					<AgentOrdersTable />
+					<ManagerOrdersTable />
 				</Box>
 			</Container>
 		</main>
