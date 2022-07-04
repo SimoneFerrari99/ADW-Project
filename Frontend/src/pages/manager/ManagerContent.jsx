d766a931ce1187fa8280eda9e7ebe88ec0f57877
SimoneFerrari99 <simone99ferrari99@gmail.com>
2022-07-04 17:21:54 +0200

    Added Agents Table on manager page

--- Frontend/src/pages/manager/ManagerContent.jsx
+++ Frontend/src/pages/manager/ManagerContent.jsx
@@ -4,6 +4,7 @@ import TabPages from "../../components/layout/Tabs/TabPages";
 
 import ManagerOrdersTable from "./components/ManagerOrdersTable";
 import ManagerCustomersTable from "./components/ManagerCustomersTable";
+import ManagerAgentsTable from "./components/ManagerAgentsTable";
 
 export default function ManagerContent() {
 	return (
@@ -11,8 +12,12 @@ export default function ManagerContent() {
 			<Container maxWidth="xl">
 				<Box sx={{ mt: { xs: 2, sm: 3 } }}>
 					<TabPages
-						tabs={[<ManagerOrdersTable />, <ManagerCustomersTable />]}
-						names={["test1", "test2"]}
+						tabs={[
+							<ManagerOrdersTable />,
+							<ManagerCustomersTable />,
+							<ManagerAgentsTable />,
+						]}
+						names={["Ordini", "Clienti", "Agenti"]}
 					/>
 				</Box>
 			</Container>
